acf5ee2818ba7307f464ae56a3ae66d95bfdb875
druga <artem.falcon@gmail.com>
2012-11-28 20:56:45 +0400

    Update watchdog/Installer/preInstall.sh

--- watchdog/Installer/preInstall.sh
+++ watchdog/Installer/preInstall.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-/usr/bin/killall 'Watchdog Feeder' 2>&1 >/dev/null
-sleep 2
-
 /bin/rm -rf '/System/Library/Extensions/iTCOWatchdog.kext' \
             '/usr/local/libexec/Watchdog Feeder' \
             '/Library/LaunchDaemons/cc.dukzcry.WatchdogFeeder.plist'
+
+/usr/bin/killall 'Watchdog Feeder' 2>&1 >/dev/null
+sleep 2
