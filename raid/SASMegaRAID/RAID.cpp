e304e6e8ac00d8d99219e22bb74115fdabdd1d33
A. V. Lukyanov <artem.falcon@gmail.com>
2013-04-03 08:30:34 +0400

    Oops.

--- raid/SASMegaRAID/RAID.cpp
+++ raid/SASMegaRAID/RAID.cpp
@@ -5,8 +5,8 @@
 #include "RAID.h"
 
 static struct cdevsw mraid_cdevsw = {
-    RAID::MRAID_Open, // lambda(dev_t, int d_open, int, struct proc *) { return d_open; }
-    RAID::MRAID_Close, // lambda(dev_t, int d_close, int, struct proc *) { return d_close; }
+    (d_open_t *)&nulldev, // lambda(dev_t, int d_open, int, struct proc *) { return d_open; }
+    (d_close_t *)&nulldev, // lambda(dev_t, int d_close, int, struct proc *) { return d_close; }
     (d_read_t *)&nulldev, // lambda(dev_t, struct uio *, int d_read) { return d_read; }
     (d_write_t *)&nulldev, // lambda(dev_t, struct uio *, int d_write) { return d_write; }
     RAID::MRAID_Ioctl, // lambda(dev_t, u_long, caddr_t, int d_ioctl, struct proc *) { return d_ioctl; }
@@ -36,14 +36,14 @@ bool RAID::init(SInt32 domain)
 {
     char str[5];
     
-    if ((devindex = cdevsw_add(-1, &mraid_cdevsw)) == -1) {
+    if ((devindex = cdevsw_add(DEVINDEX, &mraid_cdevsw)) == -1) {
         IOPrint("[RAID] cdevsw_add() failed\n");
         return false;
     }
     
     snprintf(str, sizeof(str), "mfi%d", domain);
     
-    if ((raid_devnode = devfs_make_node(makedev(DEVINDEX, 0),
+    if ((raid_devnode = devfs_make_node(makedev(devindex, 0),
                                      DEVFS_CHAR,
                                      UID_ROOT,
                                      GID_OPERATOR,
@@ -64,11 +64,11 @@ int RAID::MRAID_Ioctl(__unused dev_t dev, u_long cmd, __unused caddr_t data,
     }
 }
 
-int RAID::MRAID_Open(__unused dev_t dev, __unused int, __unused int, __unused struct proc *p)
+/*int RAID::MRAID_Open(__unused dev_t dev, __unused int, __unused int, __unused struct proc *p)
 {
     return 0;
 }
 int RAID::MRAID_Close(__unused dev_t dev, __unused int, __unused int, __unused struct proc *p)
 {
     return 0;
-}
+}*/
